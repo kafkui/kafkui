0eb66d2321147cc8775e8db44d8274bcf52fee64
j.lelong <j.lelong@groupeonepoint.com>
2021-11-14 00:35:57 +0100

    Improve log

--- src/main/java/org/peekmoon/kafkat/LogInitializer.java
+++ src/main/java/org/peekmoon/kafkat/LogInitializer.java
@@ -1,6 +1,6 @@
 package org.peekmoon.kafkat;
 
-import java.io.IOException;
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.logging.*;
@@ -20,10 +20,8 @@ public class LogInitializer {
             );
             Level defaultLevel = Level.INFO;
 
-            System.setProperty("java.util.logging.SimpleFormatter.format", "%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS.%1$tL %4$-7s [%3$s] %5$s %6$s%n");
-            logHandler.setFormatter(new SimpleFormatter() {
-
-            });
+            //System.setProperty("java.util.logging.SimpleFormatter.format", "%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS.%1$tL %4$-7s [%3$s] %5$s %6$s%n");
+            logHandler.setFormatter(new KafkatFormatter());
             logHandler.setLevel(java.util.logging.Level.FINE);
             for (Handler h : rootLogger.getHandlers()) {
                 rootLogger.removeHandler(h);
@@ -37,6 +35,33 @@ public class LogInitializer {
             throw new IllegalStateException(e);
         }
 
+    }
+
+    static class KafkatFormatter extends Formatter {
+
+        @Override
+        public String format(LogRecord record) {
+            StringBuilder builder = new StringBuilder();
+            builder
+                    .append(record.getInstant()).append(" ")
+                    .append(record.getLevel()).append(" ")
+                    .append(formatLoggerName(record.getLoggerName())).append(" - ")
+                    .append(record.getMessage())
+                    .append("\n");
+            if (record.getThrown() != null) {
+                var sw = new StringWriter();
+                var pw = new PrintWriter(sw);
+                record.getThrown().printStackTrace(pw);
+                builder.append(sw);
+            }
+            return builder.toString();
+        }
+
+        private String formatLoggerName(String loggerName) {
+            int fieldLength = 25;
+            int cutBy = loggerName.length() - fieldLength;
+            return cutBy >0 ? loggerName.substring(cutBy) : loggerName;
+        }
     }
 
 
