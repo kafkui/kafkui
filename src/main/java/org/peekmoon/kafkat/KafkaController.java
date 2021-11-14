77ab08968d093d482f33caa37fdbed0f2d15390e
j.lelong <j.lelong@groupeonepoint.com>
2021-11-14 17:08:43 +0100

    Improve kafka controller to display exceptions

--- src/main/java/org/peekmoon/kafkat/KafkaController.java
+++ src/main/java/org/peekmoon/kafkat/KafkaController.java
@@ -1,14 +1,21 @@
 package org.peekmoon.kafkat;
 
 import org.apache.kafka.clients.admin.*;
+import org.apache.kafka.common.KafkaFuture;
 import org.apache.kafka.common.config.ConfigResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 
 public class KafkaController {
+
+    private final static Logger log = LoggerFactory.getLogger(KafkaController.class);
 
     private final AdminClient client;
 
@@ -20,29 +27,47 @@ public class KafkaController {
     }
 
 
-    public void update(TopicsPage view) {
-        client.listTopics().listings().thenApply(t -> update(view, t));
-    }
-
     ///////////////////
     // Topics part
 
-    private Void update(TopicsPage view, Collection<TopicListing> topics) {
-        var configResourceList = new ArrayList<ConfigResource>();
-        var topicsName = new ArrayList<String>();
-        for (TopicListing topic : topics) {
-            String topicName = topic.name();
-            view.addTopic(topicName);
-            configResourceList.add(new ConfigResource(ConfigResource.Type.TOPIC, topicName));
-            topicsName.add(topicName);
+
+    public void update(TopicsPage view)  {
+
+        try {
+
+            var topicListing = client.listTopics().listings().get();
+
+            var configResourceList = new ArrayList<ConfigResource>();
+            var topicsName = new ArrayList<String>();
+            for (TopicListing topic : topicListing) {
+                String topicName = topic.name();
+                view.addTopic(topicName);
+                configResourceList.add(new ConfigResource(ConfigResource.Type.TOPIC, topicName));
+                topicsName.add(topicName);
+            }
+
+            KafkaFuture.allOf(
+                    client.describeTopics(topicsName).all().thenApply(r -> updateTopicsDescription(view, r)),
+                    client.describeConfigs(configResourceList).all().thenApply(r -> updateTopicsConfig(view, r))
+            ).get();
+
+        } catch (ExecutionException | InterruptedException e) {
+            throw new IllegalStateException("Unable to retrive topics data", e);
         }
 
-        client.describeConfigs(configResourceList).values()
-                .forEach((configResource, f) -> f.thenApply(conf -> view.setConfig(configResource.name(), conf)));
+    }
 
-        client.describeTopics(topicsName).values()
-                .forEach((topicName, f) -> f.thenApply(view::setDescription));
+    private Void updateTopicsConfig(TopicsPage view, Map<ConfigResource, Config> configs) {
+        for (Map.Entry<ConfigResource, Config> entry : configs.entrySet()) {
+            view.setConfig(entry.getKey().name(), entry.getValue());
+        }
+        return null;
+    }
 
+    private Void updateTopicsDescription(TopicsPage view, Map<String, TopicDescription> descriptions) {
+        for (TopicDescription description : descriptions.values()) {
+            view.setDescription(description);
+        }
         return null;
     }
 
@@ -51,19 +76,28 @@ public class KafkaController {
     // Consumers part
 
     public void update(ConsumersPage view) {
-        client.listConsumerGroups().all().thenApply(c -> update(view, c));
+        try {
+
+            client.listConsumerGroups().all()
+                    .thenApply(c -> askConsumerDescription(view, c))
+                    .get()
+                    .thenApply(c -> updateConsumer(view, c))
+                    .get();
+
+        } catch (ExecutionException | InterruptedException e) {
+            throw new IllegalStateException("Unable to retrive topics data", e);
+        }
     }
 
-    private Void update(ConsumersPage view, Collection<ConsumerGroupListing> consumers) {
+    private KafkaFuture<Map<String, ConsumerGroupDescription>> askConsumerDescription(ConsumersPage view, Collection<ConsumerGroupListing> consumers) {
         var groupeIds = consumers.stream().map(ConsumerGroupListing::groupId).collect(Collectors.toList());
-        client.describeConsumerGroups(groupeIds)
-                .describedGroups().values()
-                .forEach((descriptionFuture -> descriptionFuture.thenApply(description -> update(view, description))));
-        return null;
+        return client.describeConsumerGroups(groupeIds).all();
     }
 
-    private Void update(ConsumersPage view, ConsumerGroupDescription description) {
-        view.add(description.groupId());
+    private Void updateConsumer(ConsumersPage view, Map<String, ConsumerGroupDescription> descriptions) {
+        for (ConsumerGroupDescription description : descriptions.values()) {
+            view.add(description.groupId());
+        }
         return null;
     }
 
